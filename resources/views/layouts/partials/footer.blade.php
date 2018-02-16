85e59613157a3ed14f04c750a876d2b1670ee9f3
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-02-16 15:23:18 -0200

    Change some names

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="footer-01 col-md-3">
                 <img src="/images/parla_horizontal_fundoescuro.svg">
-                <p class="publication-name">Publicação quinzenal da Subdiretoria-Geral e Comunicação Social da Assembléia Legislativa do Estado do Rio de Janeiro
+                <p class="publication-name">Publicação quinzenal da Subdiretoria-Geral de Comunicação Social da Assembleia Legislativa do Estado do Rio de Janeiro
                 </p>
             </div>
             <div class="col-md-9 expediente">
