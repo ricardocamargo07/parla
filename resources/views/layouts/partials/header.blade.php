1f182a6d4874c80ec9776bf1555626e5c9d385fe
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-02-19 12:59:49 -0300

    Update header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,6 @@
 <div class="container header-main-inner">
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 block-logo">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 block-logo">
             <div>
                 <div id="block-sitebranding" class="margin-top-15 clearfix site-branding block block-system block-system-branding-block no-title">
                     <a href="/" title="Home" rel="home" class="site-branding-logo padding-top-20">
@@ -9,7 +9,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 header-right">
+        <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 header-right">
             <div class="header-right-inner">
                 Parla n&ordm; 1 - Fevereiro 2018
             </div>
