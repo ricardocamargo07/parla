3b5c11c73070f8c39b3a3f2f2c3968cc9c6c2465
Breno <breno@Hackintosh01.local>
2018-02-20 14:23:08 -0300

    Issue Footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="footer-01 col-md-3">
                 <img src="/images/parla_horizontal_fundoescuro.svg">
-                <p class="publication-name">Publicação quinzenal da Subdiretoria-Geral de Comunicação Social da Assembleia Legislativa do Estado do Rio de Janeiro
+                <p class="publication-name">Publicação da Subdiretoria-Geral de Comunicação Social da Assembleia Legislativa do Estado do Rio de Janeiro
                 </p>
             </div>
             <div class="col-md-9 expediente">
@@ -11,10 +11,10 @@
                 <div class="expediente-linha"><span class="expediente-label">Editor: </span><span class="expediente-names">André Coelho</span></div>
                 <div class="expediente-linha"><span class="expediente-label">Coordenação: </span><span class="expediente-names">Daniela Matta e Jorge Ramos</span></div>
                 <div class="expediente-linha"><span class="expediente-label">Equipe: </span><span class="expediente-names">Buanna Rosa, Camilla Pontes, Gustavo Natario, Isabela Cabral, Octacílio Barbosa, Tainah Vieira, Thiago Lontra, Symone Munay e Vanessa Schumacker</span></div>
-                <div class="expediente-linha"><span class="expediente-label">Edição de Arte: </span><span class="expediente-names">Daniel Tiriba e Rodrigo Cortez</span></div>
+                <div class="expediente-linha"><span class="expediente-label">Edição de Arte: </span><span class="expediente-names">Daniel Tiriba</span></div>
                 <div class="expediente-linha"><span class="expediente-label">Editor de Fotografia: </span><span class="expediente-names">Rafael Wallace</span></div>
                 <div class="expediente-linha"><span class="expediente-label">Secretária da Redação: </span><span class="expediente-names">Regina Torres</span></div>
-                <div class="expediente-linha"><span class="expediente-label">Estagiários: </span><span class="expediente-names">Carolina Moura, Danilo Gabriel (redes sociais), Elisa Calmon, Leon Lucius, Luís Gustavoc(foto) e Thiago Oliveira (redes sociais)</span></div>
+                <div class="expediente-linha"><span class="expediente-label">Estagiários: </span><span class="expediente-names">Carolina Moura, Danilo Gabriel (redes sociais), Elisa Calmon, Leon Lucius, Luís Gustavoc (foto) e Thiago Oliveira (redes sociais)</span></div>
             </div>
         </div>
         <span>@version</span><span>2018 <a href="http://www.alerj.rj.gov.br/" target="_blank" rel="noopener">Alerj</a></span>
