2c68f8e4378a5b156d4f561bf0a7920df1c3496c
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-05-10 15:35:28 -0300

    Add contact page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ Route::get('/', 'Home@index')->name('home');
 Route::get('/posts', 'Home@post')->name('post');
 
 Route::get('/posts/{year}/{month}/{day}/{slug}', 'Posts@show')->name('posts.show');
+
+Route::get('/contact', 'Contact@index')->name('contact.index');
