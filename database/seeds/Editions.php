c8d83b9cb1441682932f857665351b64a75dd08a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-06-16 12:39:28 -0300

    Rename images

--- database/seeds/Editions.php
+++ database/seeds/Editions.php
@@ -352,6 +352,8 @@ Como gestor de departamento, me especializar é sempre importante. A pós me aju
 Gabriela Naegele - Assessora Parlamentar do deputado Luiz Paulo (PSDB)
 O curso ampliou o meu leque de amizades na Casa. Passei a conhecer melhor pessoas que trabalham no administrativo, com quem tinha pouco contato. Além dos relacionamentos estreitados, a pós me fez relembrar algumas matérias da época da faculdade. Também aprendi sobre a qualidade do serviço público que é papel importante para um assessor parlamentar. Se o mestrado da Alerj sair do papel, com certeza, vou me matricular.
 
+![Gabriela Rocha](/images/photos/gabriela-naegele-citcao_elerj_thiago-lontra-1200x800.jpg)
+
 Eduardo Costa - Professor da Elerj
 Sou formado em Administração e já lecionei cursos na Elerj, então para mim foi um prazer poder retornar às salas de aula da Escola do Legislativo para o curso da pós-graduação. Estou ensinando sobre a qualidade do atendimento no setor público. Esta pós é fantástica, uma oportunidade única para o colaborador, seja comissionado ou não. É um grande diferencial preparar as pessoas que vão trabalhar com você.',
             'featured' => true
