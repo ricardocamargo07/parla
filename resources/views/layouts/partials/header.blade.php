b7d8fa60bbc67dacc8ccb0b49cdcde8bca38a5db
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-06-17 23:04:40 -0300

    Set date

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 header-right">
             <div class="header-right-inner parla-edition aligner-item">
-                Parla n&ordm; 0 - Fevereiro de 2018
+                Parla n&ordm; 1 - Junho de 2018
             </div>
         </div>
     </div>
