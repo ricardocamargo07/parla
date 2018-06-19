74e0372d47d9612b29276f6189e417b5f189fbcf
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-06-19 18:28:35 -0300

    Fix style

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.main')
 
 @section('contents')
-
-
     <div class="container text-center">
         <img  width="100"  src="/images/exclamation-triangle.svg">
 
         <h1>404</h1>
+
         <p><strong>Arquivo não encontrado</strong></p>
 
         <p>
@@ -21,18 +20,4 @@
             Ir para a página principal
         </a>
     </div>
-
-{{--
-    <div class="container">
-        <div class="row error-404">
-            <div class="col-md-offset-4 col-md-4 ">
-                <h2 class="text-center">Erro 404</h2>
-                <h3 class="text-center">A página que você tentou acessar não existe</h3>
-            </div>
-        </div>
-    </div>--}}
-
-
-
-
 @stop
